e5482c21d1d4c2068255a7e0e946f05963ea7433
Zac H <zac.haluza@gmail.com>
2020-06-21 15:04:26 -0400

    added action types

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,2 +1,7 @@
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
+
+export const GET_USERNAME = 'GET_USERNAME';
+
+export const HANDLE_PERCENT = 'HANDLE_PERCENT';
+export const HANDLE_SALARY = 'HANDLE_SALARY';
