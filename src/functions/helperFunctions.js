567b311d527fffe07e0586185cafcf1cae2cea91
Zac H <zac.haluza@gmail.com>
2020-06-21 10:27:36 -0400

    modularized function

---
+++ src/functions/helperFunctions.js
@@ -0,0 +1,15 @@
+export const formatNum = (num) => {
+  const numStr = num.toFixed(2);
+  const decimal = numStr.indexOf('.') || numStr.length - 1;
+  const arrStr = numStr.split('');
+  let count = 0;
+  for (let i = decimal; i >= 0; i--) {
+    if (count === 3) {
+      arrStr.splice(i, 0, ',');
+      count = 0;
+      i++;
+    } else count++;
+  }
+  if (arrStr[0] === ',') arrStr.splice(0, 1);
+  return arrStr.join('');
+};
