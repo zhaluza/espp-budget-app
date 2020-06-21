6e5b06e1420c135787123af587d989440abe03b7
Zac H <zac.haluza@gmail.com>
2020-06-21 10:27:06 -0400

    finished basic display logic

--- src/components/SavingsInfo/SavingsInfo.js
+++ src/components/SavingsInfo/SavingsInfo.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { formatNum } from '../../functions/helperFunctions';
 
-const SavingsInfo = (props) => {
-  return <h1>Savings!</h1>;
+const SavingsInfo = ({
+  username,
+  setUsername,
+  percent,
+  salary,
+  savings,
+  expense,
+}) => {
+  useEffect(() => {
+    const checkCookies = async () => {
+      const response = await fetch('/auth/checkSession');
+      const json = await response.json();
+      if (json.name) {
+        setUsername(json.name);
+      } else {
+        setIsLoggedIn(false);
+      }
+    };
+    checkCookies();
+  }, []);
+  return (
+    <div>
+      <h2>Thanks, {username}.</h2>
+      <div className="data">
+        <div className="data__row">
+          <p>Your Election</p>
+          <p>{percent}%</p>
+        </div>
+        <div className="data__row">
+          <p>Your Salary</p>
+          <p>${formatNum(salary)}</p>
+        </div>
+        <div className="data__row">
+          <p>Your Expense</p>
+          <p>${expense}</p>
+        </div>
+        <div className="data__row">
+          <p>Your Savings</p>
+          <p>${savings}</p>
+        </div>
+      </div>
+      <Link to="/budget">Change Election</Link>
+    </div>
+  );
 };
 
 export default SavingsInfo;
