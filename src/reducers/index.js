204f7411f26cb7d41fee648fe91255ecce2cb277
Zac H <zac.haluza@gmail.com>
2020-06-21 15:09:07 -0400

    combined reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import dataReducer from './dataReducer';
+import authReducer from './authReducer';
+
+export default combineReducers({
+  data: dataReducer,
+  auth: authReducer,
+});
