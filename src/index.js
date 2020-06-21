788c9c55517e8354fbc132649a30a3b0e1b5bf4c
Zac H <zac.haluza@gmail.com>
2020-06-20 21:04:15 -0400

    index.js boilerplate

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+import styles from '../src/styles/application.scss';
+
+render(<App />, document.getElementById('app'));
