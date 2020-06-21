1c59b89f7cdadb30977a45854211c063f8e0fa95
Zac H <zac.haluza@gmail.com>
2020-06-21 14:46:34 -0400

    wrapped app inside Provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
 import styles from '../src/styles/application.scss';
 
-render(<App />, document.getElementById('app'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
