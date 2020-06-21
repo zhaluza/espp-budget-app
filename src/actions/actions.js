9c18a3d6dda36ebe512cd999595a8abf1450a479
Zac H <zac.haluza@gmail.com>
2020-06-21 14:47:18 -0400

    added actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,3 +1,20 @@
-function login(){
-    
-}
+import * as types from '../constants/actionTypes';
+
+export const login = (username) => ({
+  type: types.LOGIN,
+  payload: username,
+});
+
+export const getUsername = () => ({
+  type: types.GET_USERNAME,
+});
+
+export const handlePercent = (percent) => ({
+  type: types.HANDLE_PERCENT,
+  payload: percent,
+});
+
+export const handleSalary = (salary) => ({
+  type: types.HANDLE_SALARY,
+  payload: salary,
+});
