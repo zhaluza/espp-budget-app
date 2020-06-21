f4018f45aefb156cd3d358948268f25a25964dcb
Zac H <zac.haluza@gmail.com>
2020-06-21 17:19:02 -0400

    imported scss, class name changes

--- src/components/BudgetPlanner/BudgetPlanner.js
+++ src/components/BudgetPlanner/BudgetPlanner.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions/actions';
 import { Link } from 'react-router-dom';
-import { formatNum } from '../../functions/helperFunctions';
+import './budgetPlanner.scss';
 
 const mapStateToProps = (state) => ({
   percent: state.data.percent,
@@ -41,35 +41,40 @@ const BudgetPlanner = ({
   }, []);
 
   return (
-    <div className="budget">
+    <div className="budget card">
       <h2>Select your monthly budget, {username}.</h2>
       <div className="budget__slider-container">
+        <p className="budget__slider-percentage">{percent}%</p>
         <input
-          className="slider"
+          className="budget__slider-input"
           type="range"
           min="1"
           max="30"
           value={percent}
           onChange={handlePercent}
         />
-        <p>{percent}%</p>
       </div>
       <div className="budget__data-container">
-        <div className="budget__data-salary">
+        <div className="budget__data-salary row">
           <p>Your Salary</p>
-          <input type="text" value={salary} onChange={handleSalary} />
+          <div className="budget__data-input-container">
+            <p>$</p>
+            <input type="number" value={salary} onChange={handleSalary} />
+          </div>
         </div>
-        <div className="budget__data-expense">
+        <div className="budget__data-expense row">
           <p>Your Expense</p>
           <p>${expense}</p>
         </div>
-        <div className="budget__data-savings">
+        <div className="budget__data-savings row">
           <p>Savings</p>
           <p>${savings}</p>
         </div>
       </div>
 
-      <Link to="/savings">Confirm Selection</Link>
+      <button className="btn small-btn">
+        <Link to="/savings">Confirm Selection</Link>
+      </button>
     </div>
   );
 };
