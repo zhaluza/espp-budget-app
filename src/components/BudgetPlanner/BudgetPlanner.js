ad68d92e46b0686b8cba1379abd453c533271d18
Zac H <zac.haluza@gmail.com>
2020-06-21 10:26:53 -0400

    basic logic w/o Redux complete

--- src/components/BudgetPlanner/BudgetPlanner.js
+++ src/components/BudgetPlanner/BudgetPlanner.js
@@ -1,27 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { formatNum } from '../../functions/helperFunctions';
 
-const BudgetPlanner = ({ username }) => {
-  const [percent, setPercent] = useState(0);
-  const [salary, setSalary] = useState(100000);
-  const [expense, setExpense] = useState(0);
-  const [savings, setSavings] = useState(0);
-
-  const formatNum = (num) => {
-    const numStr = num.toFixed(2);
-    const decimal = numStr.indexOf('.') || numStr.length - 1;
-    const arrStr = numStr.split('');
-    let count = 0;
-    for (let i = decimal; i >= 0; i--) {
-      if (count === 3) {
-        arrStr.splice(i, 0, ',');
-        count = 0;
-        i++;
-      } else count++;
-    }
-    if (arrStr[0] === ',') arrStr.splice(0, 1);
-    return arrStr.join('');
-  };
-
+const BudgetPlanner = ({
+  username,
+  setUsername,
+  setIsLoggedIn,
+  percent,
+  setPercent,
+  salary,
+  setSalary,
+  expense,
+  setExpense,
+  savings,
+  setSavings,
+}) => {
   const handleBudget = (e) => {
     setPercent(e);
     const val = e / 100;
@@ -29,17 +22,33 @@ const BudgetPlanner = ({ username }) => {
     setSavings(formatNum((salary / 12) * (1 - val)));
   };
 
+  useEffect(() => {
+    const checkCookies = async () => {
+      const response = await fetch('/auth/checkSession');
+      const json = await response.json();
+      if (json.name) {
+        setUsername(json.name);
+      } else {
+        setIsLoggedIn(false);
+      }
+    };
+    checkCookies();
+  }, []);
+
   return (
     <div className="budget">
       <h2>Select your monthly budget, {username}.</h2>
-      <input
-        className="slider"
-        type="range"
-        min="1"
-        max="30"
-        value={percent}
-        onChange={(e) => handleBudget(e.target.value)}
-      />
+      <div className="budget__slider-container">
+        <input
+          className="slider"
+          type="range"
+          min="1"
+          max="30"
+          value={percent}
+          onChange={(e) => handleBudget(e.target.value)}
+        />
+        <p>{percent}%</p>
+      </div>
       <div className="budget__data-container">
         <div className="budget__data-salary">
           <p>Your Salary</p>
@@ -54,16 +63,15 @@ const BudgetPlanner = ({ username }) => {
         </div>
         <div className="budget__data-expense">
           <p>Your Expense</p>
-          <p>
-            ${expense} ({percent}%)
-          </p>
+          <p>${expense}</p>
         </div>
         <div className="budget__data-savings">
           <p>Savings</p>
           <p>${savings}</p>
         </div>
       </div>
-      <button className="budget__button">Confirm Selection</button>
+
+      <Link to="/savings">Confirm Selection</Link>
     </div>
   );
 };
