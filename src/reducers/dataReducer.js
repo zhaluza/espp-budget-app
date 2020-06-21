51761592bdc9a890a2b5b0ecf08104a904011f8d
Zac H <zac.haluza@gmail.com>
2020-06-21 15:09:00 -0400

    handles user budget data

---
+++ src/reducers/dataReducer.js
@@ -0,0 +1,43 @@
+import * as types from '../constants/actionTypes';
+import { formatNum } from '../functions/helperFunctions';
+
+const initialState = {
+  percent: 0,
+  salary: 100000,
+  expense: 0,
+  savings: 0,
+};
+
+const dataReducer = (state = initialState, action) => {
+  let val, expense, savings;
+  switch (action.type) {
+    case types.HANDLE_PERCENT:
+      const percent = action.payload;
+      val = percent / 100;
+      expense = formatNum((state.salary / 12) * val);
+      savings = formatNum((state.salary / 12) * (1 - val));
+      return {
+        ...state,
+        percent,
+        expense,
+        savings,
+      };
+
+    case types.HANDLE_SALARY:
+      const salary = action.payload;
+      val = state.percent / 100;
+      expense = formatNum((state.salary / 12) * val);
+      savings = formatNum((state.salary / 12) * (1 - val));
+      return {
+        ...state,
+        salary,
+        expense,
+        savings,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default dataReducer;
